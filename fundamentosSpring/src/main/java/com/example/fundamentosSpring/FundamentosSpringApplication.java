0f4c190db87d3cbb896ad25771aa70124d22e57b
ddaniuwu <luis.daniel132@outlook.es>
2021-12-26 13:18:46 -0600

    spring-boot[id-001]

---
+++ fundamentosSpring/src/main/java/com/example/fundamentosSpring/FundamentosSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.fundamentosSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentosSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentosSpringApplication.class, args);
+	}
+
+}

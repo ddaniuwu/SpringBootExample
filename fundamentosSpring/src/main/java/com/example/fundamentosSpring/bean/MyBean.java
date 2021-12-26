f73c7a44a7d8815ac7a9d354685ec3d2b98b2cd9
ddaniuwu <luis.daniel132@outlook.es>
2021-12-26 16:58:49 -0600

    spring-boot(added beans configuration and implem)[id-004]

--- fundamentosSpring/src/main/java/com/example/fundamentosSpring/bean/MyBean.java
+++ fundamentosSpring/src/main/java/com/example/fundamentosSpring/bean/MyBean.java
@@ -1,5 +1,6 @@
 package com.example.fundamentosSpring.bean;
 
+//Initialize MyBean interface and prepares it to the dependency inyection
 public interface MyBean {
 	void Print();
 }
--- fundamentosSpring/src/main/java/com/example/fundamentosSpring/configuration/MyBeanConfiguration.java
+++ fundamentosSpring/src/main/java/com/example/fundamentosSpring/configuration/MyBeanConfiguration.java
@@ -6,11 +6,13 @@ import org.springframework.context.annotation.Configuration;
 import com.example.fundamentosSpring.bean.MyBean;
 import com.example.fundamentosSpring.bean.MyBeanImpl;
 
+//Configuraion for the implementation of a Bean 
 @Configuration
 public class MyBeanConfiguration {
 	
 	@Bean
 	public MyBean beanOperation() {
+		//it returns the class that implements the Bean
 		return new MyBeanImpl();
 	}
 }
